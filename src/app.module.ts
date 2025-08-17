4b785e0c8832e95ad7e8bfd83231aacde220cd56
Khaled Nadam <khalednadam@gmail.com>
2025-08-17 09:16:17 +0300

    Add .env.example file and update database configuration in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,13 +14,16 @@ import { SearchModule } from './search/search.module';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
+        port: parseInt(configService.get('DB_PORT', '5432'), 10),
+        username: configService.get('DB_USER'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         // entities: [join(process.cwd(), 'dist/**/*.entity.ts')],
         autoLoadEntities: true,
         synchronize: true,
+        ssl: {
+          rejectUnauthorized: true,
+        },
       }),
     }),
     SearchModule,
