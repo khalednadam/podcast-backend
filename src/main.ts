bf6959422ed8a893deb817259d48b1a3a1e221c0
Khaled Nadam <khalednadam@gmail.com>
2025-08-17 09:39:06 +0300

    Enable CORS in main.ts for specific origins and methods

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: [
+      'https://podcast-frontend-phi.vercel.app',
+      'http://localhost:3000',
+      'http://localhost:3001',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 }
 bootstrap();
